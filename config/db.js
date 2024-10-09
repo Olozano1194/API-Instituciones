794d03a9d18462210eafc8bee648b41e6f5de112
Oscar Lozano <olozano1194@gmail.com>
2024-10-09 01:16:59 -0500

    Se modifico el archivo vercel.json para que funcione en vercel

--- config/db.js
+++ config/db.js
@@ -4,7 +4,13 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Intentando conectar a MongoDB...');
+        console.log(`MONGO_URI: ${process.env.MONGO_URI}`);        
+        
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Conectado a la base de datos MongoDB');
     } catch (error) {
         console.error('Error al conectar a la base de datos', error);
